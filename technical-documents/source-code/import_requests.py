3acfbb73e57baa56ab256b52ef606a4bd71ba992
NewtonGold <63900235+NewtonGold@users.noreply.github.com>
2021-02-21 16:54:21 +0000

    Adding password checker

--- technical-documents/source-code/import_requests.py
+++ technical-documents/source-code/import_requests.py
@@ -6,12 +6,23 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
-        if 'login_button' in request .form: 
+        if 'login_button' in request.form: 
             username = request.form['username_input']
             hashed_psw = request.form['psw_input']
         elif 'register_button' in request.form:
             username = request.form['username_input']
             psw = request.form['psw_input']
             psw_check = request.form['psw_input_check']
+            if password_check(psw, psw_check):
+                print('Password Matches')
+            else:
+                print('Passwords do not match')
+
+
+def password_check(psw, psw_check):
+    if psw = psw_check:
+        return True
+    else:
+        return False
 
         
