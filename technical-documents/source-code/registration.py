ac6e8f15419bbd0a94e6ae1f552fba147446679b
Isaac Cheng <isaaccheng9@gmail.com>
2021-02-21 23:32:05 +0000

    Get details from registration page

--- technical-documents/source-code/registration.py
+++ technical-documents/source-code/registration.py
@@ -1,9 +1,15 @@
 """Handles the registration process for a new user account."""
 
 
+from flask.globals import request
+
+
 def validate_registration():
     """
     Validates the registration details to ensure that the email address is
     valid, and that the passwords in the form match.
     """
-    pass
+    email = request.args.get("email")
+    username = request.args.get("username")
+    password = request.args.get("password")
+    password_confirm = request.args.get("password_confirm")
