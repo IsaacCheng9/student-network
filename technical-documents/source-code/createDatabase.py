ac29d673655bcad5df87eb206ae1dfdcd67a81b9
ryanstorey999 <66261348+ryanstorey999@users.noreply.github.com>
2021-02-21 23:53:51 +0000

    Add database

---
+++ technical-documents/source-code/createDatabase.py
@@ -0,0 +1,8 @@
+import sqlite3
+
+conn = sqlite3.connect('database.db')
+c = conn.cursor()
+
+c.execute("CREATE TABLE ACCOUNTS ([username] text, [password] text, [email] text, [type] text)")
+
+conn.commit()
