18364d58a285226ff0f3435c07e2cfc015191e0d
Isaac Cheng <isaaccheng9@gmail.com>
2021-02-21 23:03:24 +0000

    Add module to handle user account registration

---
+++ technical-documents/source-code/registration.py
@@ -0,0 +1,9 @@
+"""Handles the registration process for a new user account."""
+
+
+def validate_registration():
+    """
+    Validates the registration details to ensure that the email address is
+    valid, and that the passwords in the form match.
+    """
+    pass
