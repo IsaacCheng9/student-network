971db771975e40a78e3e63fb20c4a43711dd97ee
Isaac Cheng <isaaccheng9@gmail.com>
2021-02-22 01:58:03 +0000

    Add validation for username

--- technical-documents/source-code/registration.py
+++ technical-documents/source-code/registration.py
@@ -1,5 +1,4 @@
 """Handles the registration process for a new user account."""
-
 import sqlite3
 from flask.globals import request
 from email_validator import validate_email, EmailNotValidError
@@ -18,11 +17,15 @@ def validate_registration(c) -> bool:
     Validates the registration details to ensure that the email address is
     valid, and that the passwords in the form match.
 
+    Arguments:
+        c: Cursor for the SQLite database.
+
     Returns:
         valid (bool): States whether the registration details are valid.
     """
     # Gets the user inputs from the registration page.
     # TODO: Connect function to Flask web application and test if it works.
+    valid = True
     """
     email = request.args.get("email_input")
     username = request.args.get("username_input")
@@ -37,8 +40,6 @@ def validate_registration(c) -> bool:
     password = "Password01"
     password_confirm = "Password01"
 
-    valid = True
-
     # Checks that the email address has the correct format, checks whether it
     # exists, and isn't a blacklist email.
     try:
@@ -50,13 +51,9 @@ def validate_registration(c) -> bool:
         valid = False
 
     # Checks that the username hasn't already been registered.
-    # TODO: Fix return of query not being checked properly.
-    """
-    c.execute("SELECT COUNT(username) FROM Accounts WHERE username='test123';")
-    print(c.fetchone())
-    if c.fetchone() != "(0,)":
+    c.execute("SELECT * FROM Accounts WHERE username=?;", (username,))
+    if c.fetchone() is not None:
         valid = False
-    """
 
     # Checks that the password has a minimum length of 6 characters, and at
     # least one number.
