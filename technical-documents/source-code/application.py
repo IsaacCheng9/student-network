08fed58aa8c8514a3820a6489f640ab0af44e5a3
Isaac Cheng <isaaccheng9@gmail.com>
2021-02-22 18:41:22 +0000

    Add docstrings, and clean up code

--- technical-documents/source-code/application.py
+++ technical-documents/source-code/application.py
@@ -1,5 +1,10 @@
+"""
+A student network application which is presented as a web application using
+the Flask module.
+"""
 import re
 import sqlite3
+from typing import Tuple, List
 
 from email_validator import validate_email, EmailNotValidError
 from flask import Flask, render_template, request, redirect, session
@@ -12,34 +17,54 @@ application.secret_key = ("\xfd{H\xe5 <\x95\xf9\xe3\x96.5\xd1\x01O <!\xd5\""
 
 @application.route("/", methods=["GET"])
 def index_page():
+    """
+    Redirects the user to the login page.
+
+    Returns:
+        Redirection to the login page.
+    """
     return redirect("/login")
 
 
 @application.route("/login", methods=["GET"])
 def login_page():
+    """
+    Renders the login page.
+
+    Returns:
+        The web page for user login.
+    """
     errors = []
     if "error" in session:
         errors = session["error"]
     session.pop("error", None)  # clear error session variables
     return render_template("login.html", errors=errors)
 
+
 # TODO: FOUND ERROR: CLICKING LOGIN BUTTON WITH NOTHING IN THE FORM
 @application.route("/login", methods=["POST"])
 def login_submit():
+    """
+    Validates the user's login details.
+
+    Returns:
+         Redirection depending on whether login was successful or not.
+    """
     username = request.form["username_input"]
     psw = request.form["psw_input"]
-    # Gets user from database using username.
-    # Compares password with hashed password.
-    # Compares using sha256_crypt.verify(psw, hashed_password).
+
     with sqlite3.connect("database.db") as conn:
         cur = conn.cursor()
+        # Gets user from database using username.
         cur.execute(
             "SELECT password FROM Accounts WHERE username=?;", (username,))
-        hashed_psw = cur.fetchone()[0] # <----------- line causes an error if you attempt to login with name that is not in db
+        # TODO: Line causes error for attempted login with username not in DB.
+        # Compares password with hashed password.
+        hashed_psw = cur.fetchone()[0]
         conn.commit()
         if hashed_psw is not None:
             if sha256_crypt.verify(psw, hashed_psw):
-                return redirect("/postpage")
+                return redirect("/post_page")
             else:
                 session["error"] = ["login"]
                 return redirect("/login")
@@ -56,55 +81,77 @@ def error_test():
 
 @application.route("/register", methods=["GET"])
 def register_page():
+    """
+    Renders the user registration page.
+
+    Returns:
+        The web page for user registration.
+    """
     notifs = []
     errors = ""
+
     if "notifs" in session:
         notifs = session["notifs"]
     if "error" in session:
-        errors = session['error']
+        errors = session["error"]
     session.pop("error", None)
     session.pop("notifs", None)
+
     return render_template("register.html", notifs=notifs, errors=errors)
+
 
 # TODO: FOUND ERROR: CLICKING REGISTER BUTTON WITH NOTHING IN THE FORM
 @application.route("/register", methods=["POST"])
-def register_submit():
+def register_submit() -> object:
+    """
+    Validates the user's input submitted from the registration form.
+
+    Returns:
+        The updated web page based on whether the details provided were valid.
+    """
     username = request.form["username_input"]
     password = request.form["psw_input"]
     password_confirm = request.form["psw_input_check"]
     email = request.form["email_input"]
+
     with sqlite3.connect("database.db") as conn:
         cur = conn.cursor()
-        message = []  # stores error messages to be printed to page
-        valid = False
-        valid, message = validate_registration(cur, username, password, password_confirm,
-                        email)
-        if  valid is True:
+        valid, message = validate_registration(cur, username, password,
+                                               password_confirm,
+                                               email)
+        if valid is True:
             hash_password = sha256_crypt.hash(password)
             cur.execute(
                 "INSERT INTO ACCOUNTS (username, password, email, type) "
                 "VALUES (?, ?, ?, ?);", (username, hash_password, email,
                                          "student",))
             conn.commit()
-            session['notifs'] = ['register']
+            session["notifs"] = ["register"]
             return redirect("/register")
         else:
             session["error"] = message
             return redirect("/register")
 
 
-@application.route("/postpage", methods=["GET"])
+@application.route("/post_page", methods=["GET"])
 def post_page():
+    """
+    Renders the social wall page.
+
+    Returns:
+        The web page for viewing social wall posts.
+    """
     return render_template("post_page.html")
 
 
-def validate_registration(cur, username: str, password: str,
-                          password_confirm: str, email: str) -> bool:
+def validate_registration(cur, username: str,
+                          password: str, password_confirm: str,
+                          email: str) -> Tuple[bool, List[str]]:
     """
     Validates the registration details to ensure that the email address is
     valid, and that the passwords in the form match.
 
-    Arguments:
+    Args:
         cur: Cursor for the SQLite database.
         username: The username input by the user in the form.
         password: The password input by the user in the form.
@@ -113,10 +160,13 @@ def validate_registration(cur, username: str, password: str,
         email: The email address input by the user in the form.
 
     Returns:
-        valid (bool): States whether the registration details are valid.
+        Whether the registration was valid, and the error message(s) if not.
     """
+    # Registration remains valid as long as it isn't caught by any checks. If
+    # not, error messages will be provided to the user.
     valid = True
     message = []
+
     # Checks that the email address has the correct format, checks whether it
     # exists, and isn't a blacklist email.
     try:
@@ -124,15 +174,15 @@ def validate_registration(cur, username: str, password: str,
         # Updates with the normalised form of the email address.
         email = valid_email.email
     except EmailNotValidError:
-        print("Email is invalid!")
-        valid = False
         message.append("Email is invalid!")
+        valid = False
 
     # Checks that the email address has the University of Exeter domain.
-    domain = re.search('@.*', email).group()
+    domain = re.search("@.*", email).group()
     if domain != "@exeter.ac.uk":
+        message.append(
+            "Email address does not belong to University of Exeter!")
         valid = False
-        message.append("Email address does not belong to University of Exeter!")
 
     # Checks that the username hasn't already been registered.
     cur.execute("SELECT * FROM Accounts WHERE username=?;", (username,))
@@ -154,5 +204,5 @@ def validate_registration(cur, username: str, password: str,
     return valid, message
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     application.run(debug=True)
