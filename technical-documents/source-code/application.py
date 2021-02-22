5a6ff61eaf2848cecd5884cfe93d91214e738c6c
Isaac Cheng <isaaccheng9@gmail.com>
2021-02-22 02:37:37 +0000

    Implement registration validation

--- technical-documents/source-code/application.py
+++ technical-documents/source-code/application.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, session
+from email_validator import validate_email, EmailNotValidError
 from passlib.hash import sha256_crypt
 import sqlite3
 
@@ -41,27 +42,21 @@ def register_page():
 
 @application.route('/register', methods=['POST'])
 def register_submit():
-    try:
-        username = request.form['username_input']
-        email = request.form['email_input']
-        psw = request.form['psw_input']
-        psw_check = request.form['psw_input_check']
-        if password_check(psw, psw_check):
-            print('Password Matches')
+    with sqlite3.connect('database.db') as conn:
+        cur = conn.cursor()
+        if validate_registration(cur) is True:
+            username = request.form["username_input"]
+            psw = request.form["psw_input"]
+            email = request.form["email_input"]
             hash_psw = sha256_crypt.hash(psw)
-            with sqlite3.connect('database.db') as conn:
-                cur = conn.cursor()
-                cur.execute("INSERT INTO ACCOUNTS (username,password,email,type) VALUES (?,?,?,?);",(username,hash_psw,email,'student',))
-                conn.commit()
-                conn.close()
+            cur.execute("INSERT INTO ACCOUNTS (username,password,email,type) VALUES (?,?,?,?);",(username,hash_psw,email,'student',))
+            conn.commit()
+            return redirect("/postpage")
         else:
-            print('Passwords do not match')
+            print("Registration validation failed.")
             session['error'] = ['passwordMatch']
             return redirect("/register")
-    except:
-        con.rollback()
-    finally:
-        return redirect("/postpage")
+
 
 
 @application.route('/postpage', methods=['GET'])
@@ -69,11 +64,49 @@ def post_page():
     return render_template('postpage.html')
 
 
-def password_check(psw, psw_check):
-    if psw == psw_check:
-        return True
-    else:
-        return False
+def validate_registration(cur) -> bool:
+    """
+    Validates the registration details to ensure that the email address is
+    valid, and that the passwords in the form match.
+
+    Arguments:
+        c: Cursor for the SQLite database.
+
+    Returns:
+        valid (bool): States whether the registration details are valid.
+    """
+    # Gets the user inputs from the registration page.
+    email = request.args.get("email_input")
+    username = request.args.get("username_input")
+    password = request.args.get("psw_input")
+    password_confirm = request.args.get("psw_input_check")
+    valid = True
+
+    # Checks that the email address has the correct format, checks whether it
+    # exists, and isn't a blacklist email.
+    try:
+        valid_email = validate_email(email)
+        # Updates with the normalised form of the email address.
+        email = valid_email.email
+    except EmailNotValidError:
+        print("Email is invalid!")
+        valid = False
+
+    # Checks that the username hasn't already been registered.
+    cur.execute("SELECT * FROM Accounts WHERE username=?;", (username,))
+    if cur.fetchone() is not None:
+        valid = False
+
+    # Checks that the password has a minimum length of 6 characters, and at
+    # least one number.
+    if len(password) <= 5 or any(char.isdigit() for char in password) is False:
+        valid = False
+
+    # Checks that the passwords match.
+    if password != password_confirm:
+        valid = False
+
+    return valid
 
 
 if __name__ == '__main__':
