fa2919d6183c3cd83b5404972b326f77a9005f3d
Isaac Cheng <isaaccheng9@gmail.com>
2021-03-08 03:19:38 +0000

    Add unit test for null inputs

--- technical-documents/source-code/test_social_profiles.py
+++ technical-documents/source-code/test_social_profiles.py
@@ -1,19 +1,28 @@
 import application
 
 
-def test_validate_edit_profile():
-    """Tests that profile editing validation is performed correctly."""
-    # Tests invalid details.
+def test_invalid_edit_profile():
+    """Tests that invalid profile editing details are rejected."""
     bio = ["good bio"]
-    gender = ["Male"]
-    dob = []
-    hobbies = [["good"]]
-    interests = [["good"]]
+    gender = ["bad"]
+    dob = ["2001-05-31"]
+    hobbies = [["hobby"]]
+    interests = [["interest"]]
     for num in range(len(bio)):
         valid, message = application.validate_edit_profile(
             bio[num], gender[num], dob[num], "", hobbies[num], interests[num])
         assert valid is False
 
-    # Tests valid details.
 
-    # Tests null details.
+def test_valid_edit_profile():
+    """Tests that valid profile editing details are accepted."""
+    valid, message = application.validate_edit_profile(
+        "good bio", "Male", "2001-01-31", "", ["hobby"], ["interest"])
+    assert valid is True
+
+
+def test_null_edit_profile():
+    """Tests that null profile editing details are rejected."""
+    valid, message = application.validate_edit_profile(
+        "", "", "", "", [], [])
+    assert valid is False
