ab93b6810931e09a8a83451f0ec0ceafec6ce305
Isaac Cheng <isaaccheng9@gmail.com>
2021-05-03 21:50:06 +0100

    Add staff blueprint

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ from connections import connections_blueprint
 from login import login_blueprint
 from profile import profile_blueprint
 from quizzes import quizzes_blueprint
+from staff import staff_blueprint
 
 app = Flask(__name__)
 app.register_blueprint(achievements_blueprint, url_prefix="")
@@ -12,6 +13,7 @@ app.register_blueprint(connections_blueprint, url_prefix="")
 app.register_blueprint(login_blueprint, url_prefix="")
 app.register_blueprint(profile_blueprint, url_prefix="")
 app.register_blueprint(quizzes_blueprint, url_prefix="")
+app.register_blueprint(staff_blueprint, url_prefix="")
 app.secret_key = ("\xfd{H\xe5 <\x95\xf9\xe3\x96.5\xd1\x01O <!\xd5\""
                   "xa2\xa0\x9fR\xa1\xa8")
 app.url_map.strict_slashes = False
---
+++ src/staff.py
@@ -0,0 +1,77 @@
+from flask import Blueprint, render_template, redirect
+
+from helper import *
+
+staff_blueprint = Blueprint("staff", __name__, static_folder="static",
+                            template_folder="templates")
+
+
+@staff_blueprint.route("/admin", methods=["GET", "POST"])
+def show_staff_requests() -> object:
+    """
+    Displays requests to sign up as a staff member.
+
+    Returns:
+        The web page for handling administration.
+    """
+    if "admin" in session:
+        if not session["admin"]:
+            return render_template("error.html", message=[
+                "You are not logged in to an admin account"],
+                                   requestCount=get_connection_request_count())
+        with sqlite3.connect("database.db") as conn:
+            # Loads the list of connection requests and their avatars.
+            requests = []
+            cur = conn.cursor()
+            # Extracts incoming requests.
+            cur.execute("SELECT username FROM ACCOUNTS "
+                        "WHERE type='pending_staff';")
+            conn.commit()
+            row = cur.fetchall()
+            request_count = get_connection_request_count()
+            if len(row) > 0:
+                for elem in row:
+                    requests.append(elem[0])
+
+            return render_template("admin.html", requests=requests,
+                                   requestCount=request_count)
+    else:
+        return render_template("error.html", message=[
+            "You are not logged in to an admin account"],
+                               requestCount=get_connection_request_count())
+
+
+@staff_blueprint.route("/accept_staff/<username>", methods=["GET", "POST"])
+def accept_staff(username: str):
+    """
+    Accepts user as a staff member.
+
+    Args:
+        username: The user to accept as a staff member.
+
+    Returns:
+        Redirection to the administration page.
+    """
+    with sqlite3.connect("database.db") as conn:
+        cur = conn.cursor()
+        cur.execute("UPDATE ACCOUNTS SET type=? "
+                    " WHERE username=? ;", ("staff", username))
+    return redirect("/admin")
+
+
+@staff_blueprint.route("/reject_staff/<username>", methods=["GET", "POST"])
+def reject_staff(username: str):
+    """
+    Rejects user as staff member.
+
+    Args:
+        username: The user to reject as a staff member.
+
+    Returns:
+        Redirection to the administration page.
+    """
+    with sqlite3.connect("database.db") as conn:
+        cur = conn.cursor()
+        cur.execute("UPDATE ACCOUNTS SET type=? "
+                    " WHERE username=? ;", ("student", username))
+    return redirect("/admin")
