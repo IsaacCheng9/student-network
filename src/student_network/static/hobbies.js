384946a7c8b41764df1977b11d4667aca2e38636
IsaacCheng9 <IsaacCheng9@users.noreply.github.com>
2021-05-18 02:35:18 +0000

    Prettified Code!

--- src/student_network/static/hobbies.js
+++ src/student_network/static/hobbies.js
@@ -1,73 +1,73 @@
+hTags = [];
+hobbyInput = document.getElementsByName("hobbies_input")[0];
+hValues = document.createElement("input");
+hValues.setAttribute("type", "hidden");
+hValues.setAttribute("name", "hobbies");
+hobbyInput.appendChild(hValues);
 
-	hTags = [];
-	hobbyInput = document.getElementsByName('hobbies_input')[0];
-	hValues = document.createElement('input');
-	hValues.setAttribute('type', 'hidden');
-    hValues.setAttribute('name', 'hobbies');
-	hobbyInput.appendChild(hValues);
-	
-    hobbyInput.addEventListener('input', function () {
-        let enteredhTags = hobbyInput.value.split(',');
-        if (enteredhTags.length > 1) {
-            enteredhTags.forEach(function (t) {
-                let filteredTag = filterHTag(t);
-                if (filteredTag.length > 0)
-                    addHTag(filteredTag);
-            });
-            hobbyInput.value = '';
-        }
+hobbyInput.addEventListener("input", function () {
+  let enteredhTags = hobbyInput.value.split(",");
+  if (enteredhTags.length > 1) {
+    enteredhTags.forEach(function (t) {
+      let filteredTag = filterHTag(t);
+      if (filteredTag.length > 0) addHTag(filteredTag);
     });
+    hobbyInput.value = "";
+  }
+});
 
-    hobbyInput.addEventListener('keydown', function (e) {
-        let keyCode = e.which || e.keyCode;
-        if (keyCode === 8 && hobbyInput.value.length === 0 && hTags.length > 0) {
-            removeHTag(hTags.length - 1);
-        }
-    });
+hobbyInput.addEventListener("keydown", function (e) {
+  let keyCode = e.which || e.keyCode;
+  if (keyCode === 8 && hobbyInput.value.length === 0 && hTags.length > 0) {
+    removeHTag(hTags.length - 1);
+  }
+});
 
+function addHTag(text) {
+  let tag = {
+    text: text,
+    element: document.createElement("div"),
+  };
 
-    function addHTag (text) {
-        let tag = {
-            text: text,
-            element: document.createElement('div'),
-        };
-		
-		label = document.createElement('div');
-		label.classList.add('ui', 'label');
-		label.textContent = tag.text;
-		
-        tag.element.classList.add('ui','labels', 'teal');
-		tag.element.appendChild(label);
+  label = document.createElement("div");
+  label.classList.add("ui", "label");
+  label.textContent = tag.text;
 
-        let closeBtn = document.createElement('i');
-        closeBtn.classList.add('close', 'icon');
-        closeBtn.addEventListener('click', function () {
-            removeHTag(hTags.indexOf(tag));
-        });
-        tag.element.firstElementChild.appendChild(closeBtn);
+  tag.element.classList.add("ui", "labels", "teal");
+  tag.element.appendChild(label);
 
-        hTags.push(tag);
+  let closeBtn = document.createElement("i");
+  closeBtn.classList.add("close", "icon");
+  closeBtn.addEventListener("click", function () {
+    removeHTag(hTags.indexOf(tag));
+  });
+  tag.element.firstElementChild.appendChild(closeBtn);
 
-        document.getElementsByName('hobby-div')[0].appendChild(tag.element);
+  hTags.push(tag);
 
-        refreshHTags();
-    }
+  document.getElementsByName("hobby-div")[0].appendChild(tag.element);
 
-    function removeHTag (index) {
-        let tag = hTags[index];
-        hTags.splice(index, 1);
-        tag.element.remove();
-        refreshhTags();
-    }
+  refreshHTags();
+}
 
-    function refreshHTags () {
-        let hTagsList = [];
-        hTags.forEach(function (t) {
-            hTagsList.push(t.text);
-        });
-        hValues.value = hTagsList.join(',');
-    }
+function removeHTag(index) {
+  let tag = hTags[index];
+  hTags.splice(index, 1);
+  tag.element.remove();
+  refreshhTags();
+}
 
-    function filterHTag (tag) {
-        return tag.replace(/[^\w -]/g, '').trim().replace(/\W+/g, '-');
-    }
+function refreshHTags() {
+  let hTagsList = [];
+  hTags.forEach(function (t) {
+    hTagsList.push(t.text);
+  });
+  hValues.value = hTagsList.join(",");
+}
+
+function filterHTag(tag) {
+  return tag
+    .replace(/[^\w -]/g, "")
+    .trim()
+    .replace(/\W+/g, "-");
+}
