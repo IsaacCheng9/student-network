2511485d6f9271390c137bb905de7a05aaa5ec20
BarnabasG <BarnabasG@users.noreply.github.com>
2021-05-24 01:20:01 +0100

    Remove blocked users from recommended connections

--- src/student_network/helpers/helper_connections.py
+++ src/student_network/helpers/helper_connections.py
@@ -175,15 +175,28 @@ def get_pending_connections(cur, username: str) -> list:
     )
     return cur.fetchall()
 
+def get_blocked_users(cur, username: str) -> list:
+    """
+    Gets blocked users for a user.
 
-def get_mutual_hobbies(cur, username: str, pending: list) -> dict:
+    Returns:
+        List of blocked users for a user.
+    """
+    cur.execute(
+        "SELECT user2 FROM Connection "
+        "WHERE user1=? AND connection_type='block'",
+        (username,),
+    )
+    return cur.fetchall()
+
+def get_mutual_hobbies(cur, username: str, invalid: list) -> dict:
     """
     Gets all hobbies of the user and returns a dictionary of lists of
     other users with each hobby
 
     Args:
         username: username of user to find dictionary of hobbies for
-        pending: list of users with a current pending connection, to avoid
+        invalid: list of users with a current invalid connections, to avoid
                  including these
 
     Returns:
@@ -195,21 +208,21 @@ def get_mutual_hobbies(cur, username: str, pending: list) -> dict:
     for hobby in hobbies:
         cur.execute("SELECT username FROM UserHobby WHERE hobby=?;", (hobby,))
         same_users = [
-            x[0] for x in cur.fetchall() if not x[0] == username if x[0] not in pending
+            x[0] for x in cur.fetchall() if not x[0] == username if x[0] not in invalid
         ]
         shared_users[hobby] = same_users
 
     return shared_users
 
 
-def get_mutual_interests(cur, username: str, pending: list) -> list:
+def get_mutual_interests(cur, username: str, invalid: list) -> list:
     """
     Gets all interests of the user and returns a dictionary of lists of
     other users with each interest
 
     Args:
         username: username of user to find dictionary of interests for
-        pending: list of users with a current pending connection, to avoid
+        invalid: list of users with a current invalid connections, to avoid
                  including these
 
     Returns:
@@ -221,20 +234,20 @@ def get_mutual_interests(cur, username: str, pending: list) -> list:
     for hobby in hobbies:
         cur.execute("SELECT username FROM UserInterests WHERE interest=?;", (hobby,))
         same_users = [
-            x[0] for x in cur.fetchall() if not x[0] == username if x[0] not in pending
+            x[0] for x in cur.fetchall() if not x[0] == username if x[0] not in invalid
         ]
         shared_users[hobby] = same_users
 
     return shared_users
 
 
-def get_mutual_degree(cur, username: str, pending: list, degree: str) -> list:
+def get_mutual_degree(cur, username: str, invalid: list, degree: str) -> list:
     """
     Gets a list of users who study the same degree as the passed in user
 
     Args:
         username: username of user to find list of shared degree users for
-        pending: list of users with a current pending connection, to avoid
+        invalid: list of users with a current invalid connections, to avoid
                  including
         degree: the degree of the user
 
@@ -245,7 +258,7 @@ def get_mutual_degree(cur, username: str, pending: list, degree: str) -> list:
     if degree != 1:
         cur.execute("SELECT username FROM UserProfile WHERE degree=?;", (degree,))
         shared_users = [
-            x[0] for x in cur.fetchall() if x[0] != username and x[0] not in pending
+            x[0] for x in cur.fetchall() if x[0] != username and x[0] not in invalid
         ]
 
     return shared_users
@@ -324,21 +337,21 @@ def get_recommended_connections(username: str) -> list:
     """
     with sqlite3.connect("database.db") as conn:
         cur = conn.cursor()
-        pending = [x[0] for x in get_pending_connections(cur, username)]
+        invalid = [x[0] for x in get_pending_connections(cur, username)+get_blocked_users(cur, username)]
         connections = [x[0] for x in helper_general.get_all_connections(username)]
         mutual_connections = {}
         for user in connections:
             user_cons = [x[0] for x in helper_general.get_all_connections(user)]
             for mutual in user_cons:
-                if mutual != session["username"] and mutual not in pending:
+                if mutual != session["username"] and mutual not in invalid:
                     mutual_connections = get_mutual_connections(
                         mutual_connections, mutual, user
                     )
 
-        hobbies = get_mutual_hobbies(cur, session["username"], pending)
-        interests = get_mutual_interests(cur, session["username"], pending)
+        hobbies = get_mutual_hobbies(cur, session["username"], invalid)
+        interests = get_mutual_interests(cur, session["username"], invalid)
         degree = helper_profile.get_degree(session["username"])
-        shared_degree = get_mutual_degree(cur, session["username"], pending, degree[0])
+        shared_degree = get_mutual_degree(cur, session["username"], invalid, degree[0])
 
         score_totals = calculate_similarity(
             mutual_connections, hobbies, interests, shared_degree
