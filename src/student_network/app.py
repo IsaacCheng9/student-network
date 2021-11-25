43f5295e055daf6dc31c8733e2d8f02a4bd9e426
IsaacCheng9 <isaaccheng9@gmail.com>
2021-11-25 01:27:40 +0000

    Remove src from imports

--- src/student_network/app.py
+++ src/student_network/app.py
@@ -7,15 +7,15 @@ import os
 import sqlite3
 from datetime import datetime
 
-import src.student_network.views.achievements as achievements
-import src.student_network.views.chat as chat
-import src.student_network.views.connections as connections
-import src.student_network.views.flashcards as flashcards
-import src.student_network.views.login as login
-import src.student_network.views.posts as posts
-import src.student_network.views.profile as profile
-import src.student_network.views.quizzes as quizzes
-import src.student_network.views.staff as staff
+import student_network.views.achievements as achievements
+import student_network.views.chat as chat
+import student_network.views.connections as connections
+import student_network.views.flashcards as flashcards
+import student_network.views.login as login
+import student_network.views.posts as posts
+import student_network.views.profile as profile
+import student_network.views.quizzes as quizzes
+import student_network.views.staff as staff
 from flask import Flask, request, session
 from flask_socketio import SocketIO
 
